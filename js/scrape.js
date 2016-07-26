4cab055531023aee2588e781a164e02f55065d82
Elliot Kirk <elliot.s.kirk@gmail.com>
2016-07-26 15:03:11 -0700

    Consolidate .js files

--- js/scrape.js
+++ js/scrape.js
@@ -7,3 +7,43 @@ $(document).ready(function() {
     });
   });
 });
+
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '1348890865125456',
+    xfbml      : true,
+    cookie     : true,
+    version    : 'v2.7'
+  });
+
+  FB.getLoginStatus(function(response) {
+    if (response.status === 'connected') {
+      console.log('Logged in.');
+      var accessToken = response.authResponse.accessToken;
+    }
+    else {
+      console.log('oh boy');
+      FB.login();
+    }
+  });
+
+  FB.api(
+    "/279618212103376",
+    function (response) {
+      console.log('wtf');
+      if (response && !response.error) {
+        console.log('omfg');
+      }
+      else {
+        console.log(response.error);
+      }
+    });
+  };
+
+  (function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
