f5dfec041ce85cfd515e2acc8157679fce78c804
Elliot Kirk <elliot.s.kirk@gmail.com>
2016-07-26 15:33:16 -0700

    i h8 js

--- js/scrape.js
+++ js/scrape.js
@@ -18,17 +18,16 @@ window.fbAsyncInit = function() {
         function (response) {
           console.log(response);
         });
-      };
-    }
-    else {
-      FB.login();
-    }
-  });
+      }
+      else {
+        FB.login();
+      }
+    });
 
-  (function(d, s, id){
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {return;}
-    js = d.createElement(s); js.id = id;
-    js.src = "https://connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
+    (function(d, s, id){
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {return;}
+      js = d.createElement(s); js.id = id;
+      js.src = "https://connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
