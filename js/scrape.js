84e96f8720e70a24e202097a470e6683621621b4
Elliot Kirk <elliot.s.kirk@gmail.com>
2016-07-27 10:52:26 -0700

    Hmm

--- js/scrape.js
+++ js/scrape.js
@@ -36,12 +36,12 @@ window.fbAsyncInit = function() {
           });
         }
       });
-    }
+    };
 
-      (function(d, s, id){
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) {return;}
-        js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/en_US/sdk.js';
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));
+    (function(d, s, id){
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {return;}
+      js = d.createElement(s); js.id = id;
+      js.src = 'https://connect.facebook.net/en_US/sdk.js';
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
