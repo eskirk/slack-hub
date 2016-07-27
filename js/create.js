1cb2bc77878ea427a4095c399ede0ffb8d10448f
Elliot Kirk <elliot.s.kirk@gmail.com>
2016-07-27 10:43:48 -0700

    Added files

---
+++ js/create.js
@@ -0,0 +1,14 @@
+var createCard = function() {
+  var htmlText = `
+    <div class='card'>
+      <div class='row'>
+        <div class='col-md-11'>
+          <div class='card blue-grey darken-1'>
+            <span class='card-title'>Post</span>
+            <p>Test text tastes like chex</p>
+          </div>
+        </div>
+      </div>
+    </div>`;
+  return htmlText;
+};
