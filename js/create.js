2994490dda9d07c83ee7f8617034f04dc4a7b63b
Elliot Kirk <elliot.s.kirk@gmail.com>
2016-07-27 15:49:56 -0700

    Cards n shtuff

--- js/create.js
+++ js/create.js
@@ -1,11 +1,36 @@
 var createCard = function() {
   var htmlText = `
-    <div class='card'>
+    <div class='card-container'>
       <div class='row'>
-        <div class='col-md-11'>
-          <div class='card blue-grey darken-1'>
-            <span class='card-title'>Post</span>
-            <p>Test text tastes like chex</p>
+        <div class='col-md-5'>
+          <div class='card listing blue-grey darken-1'>
+            <div class='card-content white-text'>
+              <span class='card-title'>Post</span>
+              <p>Test text tastes like chex</p>
+            </div>
+            <div class='card-action'>
+              <a class='card-link' href="javascript:$(\'.card-holder\').prepend(createCard());">Create another!</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>`;
+  return htmlText;
+};
+
+var createListingCard = function(price, listing) {
+  var htmlText = `
+    <div class='card-container'>
+      <div class='row'>
+        <div class='col-md-5'>
+          <div class='card listing blue-grey darken-1'>
+            <div class='card-content white-text'>
+              <span class='card-title'>` + price + `</span>
+              <p>` + listing + `</p>
+            </div>
+            <div class='card-action'>
+              <a class='card-link' href="javascript:$(\'.card-holder\').prepend(createCard());">Create another!</a>
+            </div>
           </div>
         </div>
       </div>
