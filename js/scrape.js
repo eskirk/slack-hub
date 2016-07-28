4bb547f59e805f76096c3f8dc226399adb8a0e58
Elliot Kirk <elliot.s.kirk@gmail.com>
2016-07-28 16:48:45 -0700

    logic!

--- js/scrape.js
+++ js/scrape.js
@@ -24,7 +24,7 @@ window.fbAsyncInit = function() {
               $('.card-holder').prepend(createListingCard(findPrice(response.feed.data[i].message), response.feed.data[i].message));
             }
           }
-          showStaggeredList('#card-list-item');
+          Materialize.showStaggeredList('#card-list-item');
         });
       }
       else {
